20e888f8c24b349b0f234ab5234e7b54b505af1d
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-01-15 23:32:01 -0600

    added firebase authentication

--- app/src/main/java/com/example/smartadns/RegisterUSer.java
+++ app/src/main/java/com/example/smartadns/RegisterUSer.java
@@ -3,12 +3,38 @@ package com.example.smartadns;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterUSer extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    private EditText registerFirstName;
+    private EditText registerLastName;
+    private EditText registerEmailId;
+    private EditText registerPassword;
+    private EditText registerProductId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        registerFirstName = findViewById(R.id.registerFirstName);
+        registerLastName = findViewById(R.id.registerLastName);
+        registerEmailId = findViewById(R.id.registerEmailId);
+        registerPassword  = findViewById(R.id.registerPassword);
+        registerProductId = findViewById(R.id.registerProductId);
+
+
+        
+
+
     }
 }
---
+++ app/src/main/java/com/example/smartadns/controller.java
@@ -0,0 +1,14 @@
+package com.example.smartadns;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class controller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controller);
+    }
+}
