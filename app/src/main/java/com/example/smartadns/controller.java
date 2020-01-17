ffbe0aaf523638809dc74e3ce967ca3a8970b503
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-01-16 20:01:22 -0600

    Added map, contact and contact update fragments

--- app/src/main/java/com/example/smartadns/controller.java
+++ app/src/main/java/com/example/smartadns/controller.java
@@ -1,14 +1,24 @@
 package com.example.smartadns;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
-public class controller extends AppCompatActivity {
+public class Controller extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
